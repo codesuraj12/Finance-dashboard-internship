7d269cf1f3d453d47fbbd520f7a826d5fcfbb090
codesuraj12 <160992864+codesuraj12@users.noreply.github.com>
2026-04-08 19:42:45 +0530

    updated sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -3,47 +3,48 @@ import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div className="w-64 h-screen bg-cyan-800  shadow-lg  flex flex-col">
+    <div className="w-64 h-screen bg-[#111827] shadow-lg  flex flex-col">
       <h2 className="text-2xl font-bold mb-8 text-white tracking-wide p-6 border-b-1 border-blue-900 shadow-lg">Finance</h2>
 
       <ul className="space-y-4  px-6">
-        <li className="hover:text-blue-500 font-semibold flex gap-2   p-2">
+        <li >
           <NavLink
             to="/"
-            className={({ isActive }) =>
-              `flex items-center gap-3 
-              ${isActive
-                ? " text-cyan-200 font-bold"
-                : " text-white font-semibold hover:text-white"
-              }`}
+             className={({ isActive }) =>
+          `flex items-center gap-3 p-3 rounded-lg transition-all
+          ${isActive
+            ? "bg-[#1F2937] text-cyan-400" 
+            : "text-gray-400 hover:bg-[ #0F172A] hover:text-white"
+          }`
+        }
           >
          <i className="fa-solid fa-house"></i>    Overview
           </NavLink>
         </li>
-        <li className="hover:text-blue-500 cursor-pointer  p-2">
+        <li >
           <NavLink
             to="/transactions"
-            className={({ isActive }) =>
-              `flex items-center gap-3 
-              ${isActive
-                ? " text-cyan-200 font-bold"
-                : " text-white font-semibold hover:text-white"
-              }`
-            }
+             className={({ isActive }) =>
+          `flex items-center gap-3 p-3 rounded-lg transition-all
+          ${isActive
+            ? "bg-[#1F2937] text-cyan-400"
+            : "text-gray-400 hover:bg-[#0F172A] hover:text-white"
+          }`
+        }
           >
          <i className="fa-solid fa-list"></i>     Transactions
           </NavLink>
         </li>
-        <li className="hover:text-blue-500 cursor-pointer p-2">
+        <li >
           <NavLink
             to="/insights"
-            className={({ isActive }) =>
-              `flex items-center gap-3 
-              ${isActive
-                ? " text-cyan-200 font-bold"
-                : " text-white font-semibold hover:text-white"
-              }`
-            }
+             className={({ isActive }) =>
+          `flex items-center gap-3 p-3 rounded-lg transition-all
+          ${isActive
+            ? "bg-[#1F2937] text-cyan-400"
+            : "text-gray-400 hover:bg-[#0F172A] hover:text-white"
+          }`
+        }
           >
           <i className="fa-solid fa-chart-line"></i>   Insights
           </NavLink>
